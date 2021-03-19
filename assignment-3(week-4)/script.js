418af4cf0263e589b7fc9def65c439f71b9102a5
guruprasad reddy <gpreddy2526@gmail.com>
2021-03-19 13:00:09 +0530

    all

--- assignment-3(week-4)/script.js
+++ assignment-3(week-4)/script.js
@@ -1,11 +1,24 @@
-(function () {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names=new Array();
+names [0] ="Yaakov" ;
+      names [1]="John";
+       names [2]="Jen";
+       names [3]="Jason";
+        names [4]="Paul" ;
+        names [5]="Frank";
+        names [6]="Larry";
+         names [7]="Paula";        
+         names [8]="laura";
+         names [9]="jim";
 
-  for (var index in names) {
-    if (names[index].toLowerCase().startsWith("j")) {
-      byeSpeaker.speak(names[index]);
-    } else {
-      helloSpeaker.speak(names[index]);
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      if (names[i].charAt(0)==='j'|| names[i].charAt(0) === 'J') {
+        console.log("GoodBye"+ names[i])
+      } 
+      else {
+       console.log("Hello"+ names[i])
+      }
     }
-  }
-})();
+    
+   
